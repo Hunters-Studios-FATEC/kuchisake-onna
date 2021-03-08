e11193fc81afd11fd97bd09331183cca6eb270e7
PedroKazi <69813674+PedroKazi@users.noreply.github.com>
2021-03-08 11:22:36 -0300

    first commit

---
+++ core/src/com/hunter/game/kuchisake/Game.java
@@ -0,0 +1,22 @@
+package com.hunter.game.kuchisake;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Game extends ApplicationAdapter {
+	
+	@Override
+	public void create () {
+	}
+
+	@Override
+	public void render () {
+	}
+	
+	@Override
+	public void dispose () {
+	}
+}
